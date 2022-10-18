872205bb298dc2169b6673520023c300f616b3e5
NAGABUSHAN <NAGABUSHAN@DESKTOP-T3DFI5P>
2022-10-18 20:27:56 +0530

    first commit

---
+++ B28/src/org/tns/program/Bike.java
@@ -0,0 +1,33 @@
+package org.tns.program;
+
+public class Bike
+{
+	void displaynames()
+	{
+		System.out.println("Following models of the bikes are available ");
+	
+	}
+}
+class Seden extends Bike
+{
+	void model1()
+	{
+		System.out.println("Himalayan");
+		
+	}
+}
+class Hatchback extends Bike
+{
+	void model2()
+	{
+		System.out.println("Yamaha");
+		
+	}
+}
+class Suv extends Bike
+{
+	void model3()
+	{
+		System.out.println("Honda");
+	}
+}
--- B28/src/org/tns/program/Multiples.java
+++ B28/src/org/tns/program/Multiples.java
@@ -5,13 +5,13 @@ public class Multiples {
 public static void main(String[] args) {
 Scanner reader = new Scanner(System.in);
 System.out.print("Enter two numbers: ");
-// nextDouble() reads the next double from the keyboard
+
 double first = reader.nextDouble();
 double second = reader.nextDouble();
 System.out.print("Enter an operator (+, -, *, /): ");
 char operator = reader.next().charAt(0);
 double result;
-//switch case for each of the operations
+
 switch(operator)
 {
 case '+':
@@ -26,14 +26,12 @@ break;
 case '/':
 result = first / second;
 break;
-// operator doesn't match any case constant (+, -, *, /)
- 
  
 default:
 System.out.printf("Error! operator is not correct");
 return;
 }
-//printing the result of the operations
+
 System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
 }
 }
