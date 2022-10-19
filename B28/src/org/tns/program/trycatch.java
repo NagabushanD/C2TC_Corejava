9581a306aea6f899576eed002c0e5c91b8387939
NAGABUSHAN <NAGABUSHAN@DESKTOP-T3DFI5P>
2022-10-19 21:00:14 +0530

    first commit

---
+++ B28/src/org/tns/program/trycatch.java
@@ -0,0 +1,13 @@
+package org.tns.program;
+
+public class trycatch {
+
+	 public static void main(String[ ] args) {
+		    try {
+		      int[] myNumbers = {1, 2, 3};
+		      System.out.println(myNumbers[10]);
+		    } catch (Exception e) {
+		      System.out.println("Something went wrong.");
+		    }
+		  }
+		}
